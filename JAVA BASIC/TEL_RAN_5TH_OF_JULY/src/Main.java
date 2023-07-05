fc4deed70c362d16d24dbb176c0d01a86267dbc7
Tatiana Kamenets <137260798+tatkakam@users.noreply.github.com>
2023-07-06 00:10:47 +0200

    first HW JB 06/07/2023

---
+++ JAVA BASIC/TEL_RAN_5TH_OF_JULY/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String...args) {
+        String text1 = "Мне очень ";
+        String text2 = "нравится учиться новому! ";
+        String text3 = text1 + text2 ;
+        System.out.println(text1);
+        System.out.println(text2);
+        System.out.println(text3);
+        int number1 = 900;
+        int number2 = 3;
+        int number3 = 100;
+        int number4 = number1 + number3;
+        int number5 = number4 / number3 - number2;
+        int number6 = number2 * (number1 - number3);
+        System.out.println(number4);
+        System.out.println(number5);
+        System.out.println(number6);
+           }
+}
