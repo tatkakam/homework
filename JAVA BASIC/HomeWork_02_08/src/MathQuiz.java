17c869b111802c45af38a31494725fa34aa7548b
Tatiana Kamenets <137260798+tatkakam@users.noreply.github.com>
2023-08-15 20:32:08 +0200

    Update MathQuiz.java

--- JAVA BASIC/HomeWork_02_08/src/MathQuiz.java
+++ JAVA BASIC/HomeWork_02_08/src/MathQuiz.java
@@ -12,7 +12,7 @@ public class MathQuiz {
         int x = 0;
         int y = 0;
 
-        int counter = 0;
+        int score = 0;
 
         do {
             x = randomizer.nextInt(9) + 1;
@@ -21,36 +21,36 @@ public class MathQuiz {
             System.out.println("Сложите эти два случайных числа " + x + "+" + y + " и напишите ответ: ");
             userAnswer= sc.nextInt();
             correctAnswer = Calculator.add(x, y);
-            if (correctAnswer != userAnswer) { counter = counter - 5 ;}
-            else {counter = counter + 5;}
-            System.out.println("Ваш ответ: " + userAnswer + ". Правильный ответ: " + correctAnswer + ". Текущее количество баллов: " + counter);
+            if (correctAnswer != userAnswer) { score = score - 5 ;}
+            else {score = score + 5;}
+            System.out.println("Ваш ответ: " + userAnswer + ". Правильный ответ: " + correctAnswer + ". Текущее количество баллов: " + score);
 
             System.out.println("Произведите вычитание этих двух случайных чисел " + x + "-" + y + " и напишите ответ: ");
             userAnswer= sc.nextInt();
             correctAnswer = Calculator.substract(x, y);
-            if (correctAnswer != userAnswer) { counter = counter - 5 ;}
-            else {counter = counter + 5;}
-            System.out.println("Ваш ответ: " + userAnswer + ". Правильный ответ: " + correctAnswer + ". Текущее количество баллов: " + counter);
+            if (correctAnswer != userAnswer) { score = score - 5 ;}
+            else {score = score + 5;}
+            System.out.println("Ваш ответ: " + userAnswer + ". Правильный ответ: " + correctAnswer + ". Текущее количество баллов: " + score);
 
             System.out.println("Перемножьте эти два случайных числа " + x + "*" + y + " и напишите ответ: ");
             userAnswer = sc.nextInt();
             correctAnswer = Calculator.multyply(x, y);
-            if (correctAnswer != userAnswer) { counter = counter - 5 ;}
-            else {counter = counter + 5;}
-            System.out.println("Ваш ответ: " + userAnswer + ". Правильный ответ: " + correctAnswer + ". Текущее количество баллов: " + counter);
+            if (correctAnswer != userAnswer) { score = score - 5 ;}
+            else {score = score + 5;}
+            System.out.println("Ваш ответ: " + userAnswer + ". Правильный ответ: " + correctAnswer + ". Текущее количество баллов: " + score);
 
             System.out.println("Поделите эти два случайных числа " + x + "/" + y + " и напишите ответ в виде целой части: ");
             userAnswer= sc.nextInt();
             correctAnswer = Calculator.divide(x, y);
-            if (correctAnswer != userAnswer) { counter = counter - 5 ;}
-            else {counter = counter + 5;}
-            System.out.println("Ваш ответ: " + userAnswer + ". Правильный ответ: " + correctAnswer + ". Текущее количество баллов: " + counter);
+            if (correctAnswer != userAnswer) { score = score - 5 ;}
+            else {score = score + 5;}
+            System.out.println("Ваш ответ: " + userAnswer + ". Правильный ответ: " + correctAnswer + ". Текущее количество баллов: " + score);
                break;
         } while (correctAnswer != userAnswer);
 
-        if (counter < 20)
-        {System.out.println ("Ваш финальный счет: " + counter + " баллов. Вы допустили ошибки, можете попробовать заново в следующий раз. Удачи!");}
-        else {System.out.println("Ура! Опросник завершён! Вы победили!  Ваш финальный счет составил  максимальное значение: " + counter);}
+        if (score < 20)
+        {System.out.println ("Ваш финальный счет: " + score + " баллов. Вы допустили ошибки, можете попробовать заново в следующий раз. Удачи!");}
+        else {System.out.println("Ура! Опросник завершён! Вы победили!  Ваш финальный счет составил  максимальное количество: " + score + " баллов.");}
 
         return correctAnswer;
     }
