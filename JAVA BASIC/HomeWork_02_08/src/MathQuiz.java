bd2aba61a5e8d34ddcb8238df6ab38af088f1b2b
Tatiana Kamenets <137260798+tatkakam@users.noreply.github.com>
2023-08-15 20:17:57 +0200

    Update MathQuiz.java
    убрала многие переменные ненужные.

--- JAVA BASIC/HomeWork_02_08/src/MathQuiz.java
+++ JAVA BASIC/HomeWork_02_08/src/MathQuiz.java
@@ -8,84 +8,50 @@ public class MathQuiz {
 
         int correctAnswer = 0;
         int userAnswer = 1;
-        int correctAnswerAdd = 0;
-        int userAnswerAdd = 1;
-        int correctAnswerSubstract;
-        int userAnswerSubstract;
-        int correctAnswerMultyPly;
-        int userAnswerMultyPly;
-        int correctAnswerDivide;
-        int userAnswerDivide;
-
 
         int x = 0;
         int y = 0;
 
         int counter = 0;
-        int counterAdd = 0;
-        int counterSubstract = 0;
-        int counterMultyPly = 0;
-        int counterDivide = 0;
-        int finalCounter = 0;
 
         do {
             x = randomizer.nextInt(9) + 1;
             y = randomizer.nextInt(9) + 1;
 
             System.out.println("Сложите эти два случайных числа " + x + "+" + y + " и напишите ответ: ");
-            userAnswerAdd= sc.nextInt();
-            userAnswer = userAnswerAdd;
-            correctAnswerAdd = Calculator.add(x, y);
-            correctAnswer = correctAnswerAdd;
-            if (correctAnswerAdd != userAnswerAdd) { counter = counter - 5 ;
-            counterAdd = counter;}
-            else {counter = counter + 5;
-            counterAdd = counter;}
-            System.out.println("Ваш ответ: " + userAnswer + ". Правильный ответ: " + correctAnswer + ". Текущее количество баллов: " + counterAdd);
+            userAnswer= sc.nextInt();
+            correctAnswer = Calculator.add(x, y);
+            if (correctAnswer != userAnswer) { counter = counter - 5 ;}
+            else {counter = counter + 5;}
+            System.out.println("Ваш ответ: " + userAnswer + ". Правильный ответ: " + correctAnswer + ". Текущее количество баллов: " + counter);
 
             System.out.println("Произведите вычитание этих двух случайных чисел " + x + "-" + y + " и напишите ответ: ");
-            userAnswerSubstract= sc.nextInt();
-            userAnswer = userAnswerSubstract;
-            correctAnswerSubstract = Calculator.substract(x, y);
-            correctAnswer = correctAnswerSubstract;
-            if (correctAnswerSubstract != userAnswerSubstract) { counter = counter - 5 ;
-                counterSubstract = counter;}
-            else {counter = counter + 5;
-                counterSubstract = counter;}
-            System.out.println("Ваш ответ: " + userAnswer + ". Правильный ответ: " + correctAnswer + ". Текущее количество баллов: " + counterSubstract);
+            userAnswer= sc.nextInt();
+            correctAnswer = Calculator.substract(x, y);
+            if (correctAnswer != userAnswer) { counter = counter - 5 ;}
+            else {counter = counter + 5;}
+            System.out.println("Ваш ответ: " + userAnswer + ". Правильный ответ: " + correctAnswer + ". Текущее количество баллов: " + counter);
 
             System.out.println("Перемножьте эти два случайных числа " + x + "*" + y + " и напишите ответ: ");
-            userAnswerMultyPly = sc.nextInt();
-            userAnswer = userAnswerMultyPly;
-            correctAnswerMultyPly = Calculator.multyply(x, y);
-            correctAnswer = correctAnswerMultyPly;
-            if (correctAnswerMultyPly != userAnswerMultyPly) { counter = counter - 5 ;
-                counterMultyPly = counter;}
-            else {counter = counter + 5;
-                counterMultyPly = counter;}
-            System.out.println("Ваш ответ: " + userAnswer + ". Правильный ответ: " + correctAnswer + ". Текущее количество баллов: " + counterMultyPly);
+            userAnswer = sc.nextInt();
+            correctAnswer = Calculator.multyply(x, y);
+            if (correctAnswer != userAnswer) { counter = counter - 5 ;}
+            else {counter = counter + 5;}
+            System.out.println("Ваш ответ: " + userAnswer + ". Правильный ответ: " + correctAnswer + ". Текущее количество баллов: " + counter);
 
             System.out.println("Поделите эти два случайных числа " + x + "/" + y + " и напишите ответ в виде целой части: ");
-            userAnswerDivide= sc.nextInt();
-            userAnswer = userAnswerDivide;
-            correctAnswerDivide = Calculator.divide(x, y);
-            correctAnswer = correctAnswerDivide;
-            if (correctAnswerDivide != userAnswerDivide) { counter = counter - 5 ;
-                counterDivide = counter;}
-            else {counter = counter + 5;
-                counterDivide = counter;}
-            System.out.println("Ваш ответ: " + userAnswer + ". Правильный ответ: " + correctAnswer + ". Текущее количество баллов: " + counterDivide);
-            if (counterDivide < 15 || counterAdd < 0 || counterMultyPly < 10 || counterSubstract < 5)
-            { System.out.println ("Вы допустили ошибки, можете попробовать заново в следующий раз. Удачи!");}
-            if (counterDivide < counterMultyPly) {break;}
+            userAnswer= sc.nextInt();
+            correctAnswer = Calculator.divide(x, y);
+            if (correctAnswer != userAnswer) { counter = counter - 5 ;}
+            else {counter = counter + 5;}
+            System.out.println("Ваш ответ: " + userAnswer + ". Правильный ответ: " + correctAnswer + ". Текущее количество баллов: " + counter);
+               break;
         } while (correctAnswer != userAnswer);
 
-        finalCounter = counterDivide;
-
-        if (finalCounter >= 20) {
-            System.out.println("Ура! Опросник завершён! Вы победили!  Ваш финальный счет составил  максимальное значение: " + finalCounter);}
+        if (counter < 20)
+        {System.out.println ("Ваш финальный счет: " + counter + " баллов. Вы допустили ошибки, можете попробовать заново в следующий раз. Удачи!");}
+        else {System.out.println("Ура! Опросник завершён! Вы победили!  Ваш финальный счет составил  максимальное значение: " + counter);}
 
         return correctAnswer;
-
     }
 }
