a433aa60d8720aafbf55395fd6375e4c171cd4c3
Tatiana Kamenets <137260798+tatkakam@users.noreply.github.com>
2023-08-22 14:08:15 +0200

    Update BubbleSortExample.java сорт массивов с именами

--- JAVA BASIC/HW_09_08/src/BubbleSortExample.java
+++ JAVA BASIC/HW_09_08/src/BubbleSortExample.java
@@ -1,6 +1,8 @@
 public class BubbleSortExample {
-    public static void bubbleSort(int[] scores) {
+    public static void bubbleSort(int[] scores, String[] firstNames, String[] lastNames) {
         int temp = 0;
+        String fn;
+        String ln;
         for (int i = 0; i < 3; i++) {
             for (int j = 1; j < (3 - i); j++) {
                 if (scores[j - 1] > scores[j]) {
@@ -8,6 +10,14 @@ public class BubbleSortExample {
                     temp = scores[j - 1];
                     scores[j - 1] = scores[j];
                     scores[j] = temp;
+
+                    fn = firstNames[j - 1];
+                    firstNames[j - 1] = firstNames[j];
+                    firstNames[j] = fn;
+
+                    ln = lastNames[j - 1];
+                    lastNames[j - 1] = lastNames[j];
+                    lastNames[j] = ln;
                 }
             }
         }
